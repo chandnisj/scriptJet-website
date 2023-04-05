11dabf6c84336b91c074cf88d8759048a4eb4fc4
chandnisj <chandani.scriptjet@gmail.com>
2023-04-05 18:54:23 +0530

    added

--- src/Screen/Home.jsx
+++ src/Screen/Home.jsx
@@ -3,8 +3,27 @@ import { Link } from "react-router-dom";
 import home from "../Screen/Images/home.jpg";
 import h2 from "../Screen/Images/home-2.jpg";
 import aboutus from "../Screen/Images/about.jpg";
-
+import home3 from "../Screen/Images/home-3.jpg";
 function Home() {
+  // <div className="flex font-serif" style={{ position: "relative"}}>
+  //     <img
+  //       src={home3}
+  //       alt="home-3"
+  //       srcset=""
+  //       className=" backdrop-blur-xl"
+  //       style={{filter:"contrast(1) brightness(50%)"}}
+  //     />
+  //     <div className="flex-col sm:flex-row items-center justify-start absolute h-full  w-full py-20   ml-3 sm:ml-4  ">
+  //       <h1 className="text-start text-white  font-bold  text-2xl">
+  //         WHY US
+  //       </h1>
+  //       <div className="container min-w-1/4 flex-col sm:flex-row  text-black">
+  //       <p className="text-lg">
+  //       Our Consulting agency provides Consulting,ideas,and resources for people Working to create social change.we bring the right people together to challenge established thinking and drive transformation.We work with our client to build the capabilities that enable organizations to achieve sustainable advantage
+  //      </p>
+  //       </div>
+  //      </div>
+  //     </div>
   return (
     <div className="">
       <section className=" text-gray-600  body-font ">
@@ -47,119 +66,97 @@ function Home() {
               Our Service
             </h1>
           </div>
-          <div className="grid lg:grid-cols-4  md:grid-cols-2 sm:grid-cols-2  grid-cols-1 -m-2">
+          <div className="grid lg:grid-cols-4  md:grid-cols-2 sm:grid-cols-2  grid-cols-1 -m-2 ">
             <div className="p-2 ">
               <div className=" p-4 rounded-lg border-2 border-gray-100 flex flex-col relative overflow-hidden bg-blue-50">
-                <div className="sm:text-center text-center md:text-start">
-                  <h1 className="text-2xl font-semibold  text-center   ">
-                    UI/UX Design
-                  </h1>
+                <div className="sm:text-center text-center md:text-center h-full">
+                  <Link to="/Technologies" className=" text-2xl font-semibold ">
+                    Web Developement
+                  </Link>
                   <p className="flex items-center text-center mt-4 text-lg text-[#14406D] mb-8">
                     Let us take you into a deeper experience, make a moment a
                     lasting conveyable memory. .
                   </p>
                 </div>
-                <div className="flex sm:w-50 items-center justify-center">
-                  <Link
-                    to="/Contact-Us"
-                    className="  text-center   bg-[#14406D] hover:bg-gray-400 hover:text-black text-white font-serif  text-lg border-0 py-1 px-3 focus:outline-none rounded-tr-xl rounded-bl-xl  mt-0"
-                  >
-                    Contact Us
-                  </Link>
-                </div>
               </div>
             </div>
-
-            <div className="p-2">
-              <div className=" p-4 rounded-lg border-2 border-gray-100 flex flex-col relative overflow-hidden bg-blue-50">
-                <div className="sm:text-center text-center md:text-start">
-                  <h1 className="text-2xl font-semibold text-center">
-                    Web Devlopement
-                  </h1>
+            <div className="p-2 ">
+              <div className=" p-4 rounded-lg border-2 border-gray-100 flex  flex-col relative overflow-hidden bg-blue-50">
+                <div className="sm:text-center text-center md:text-center">
+                  <Link
+                    to="/Technologies"
+                    className="text-2xl font-semibold text-center"
+                  >
+                    Mobile App Devlopement
+                  </Link>
                   <p className="flex items-center text-center mt-4 text-lg text-[#14406D]  mb-8">
                     Let us take you into a deeper experience, make a moment a
                     lasting conveyable memory. .
                   </p>
                 </div>
-                <div className="flex  sm:w-50 items-center justify-center">
-                  <Link
-                    to="/Contact-Us"
-                    className="text-center    bg-[#14406D] hover:bg-gray-400 hover:text-black text-white font-serif  text-lg border-0 py-1 px-3 focus:outline-none rounded-tr-xl rounded-bl-xl  mt-0"
-                  >
-                    Contact Us
-                  </Link>
-                </div>
               </div>
             </div>
-
             <div className="p-2">
               <div className=" p-4 rounded-lg border-2 border-gray-100 flex flex-col relative overflow-hidden bg-blue-50">
-                <div className="sm:text-center text-center md:text-start">
-                  <h1 className="text-2xl font-semibold text-center  ">
-                    Mobile Devlopement
-                  </h1>
+                <div className="sm:text-center text-center md:text-center">
+                  <Link
+                    to="/Technologies"
+                    className="text-2xl font-semibold text-center  "
+                  >
+                    UI/Ux Design
+                  </Link>
                   <p className="flex items-center text-center mt-4 text-lg text-[#14406D]  mb-8">
                     Let us take you into a deeper experience, make a moment a
                     lasting conveyable memory. .
                   </p>
                 </div>
-                <div className="flex  sm:w-50 items-center justify-center">
-                  <Link
-                    to="/Contact-Us"
-                    className="text-center   bg-[#14406D] hover:bg-gray-400 hover:text-black text-white font-serif  text-lg border-0 py-1 px-3 focus:outline-none rounded-tr-xl rounded-bl-xl  mt-0"
-                  >
-                    Contact Us
-                  </Link>
-                </div>
               </div>
             </div>
-
             <div className="p-2 ">
               <div className=" p-4 rounded-lg border-2 border-gray-100 flex flex-col relative overflow-hidden bg-blue-50">
-                <div className="sm:text-center text-center md:text-start">
-                  <h1 className="text-2xl font-semibold text-center  ">
+                <div className="sm:text-center text-center md:text-center">
+                  <Link
+                    to="/Technologies"
+                    className="text-2xl font-semibold text-center  "
+                  >
                     QA and Testing
-                  </h1>
+                  </Link>
                   <p className="flex items-center text-center mt-4 text-lg text-[#14406D] overflow-hidden  mb-8">
                     Let us take you into a deeper experience, make a moment a
                     lasting conveyable memory. .
                   </p>
                 </div>
-                <div className="flex sm:w-50 items-center justify-center">
-                  <Link
-                    to="/Contact-Us"
-                    className="text-center   bg-[#14406D] hover:bg-gray-400 hover:text-black text-white font-serif  text-lg border-0 py-1 px-3 focus:outline-none rounded-tr-xl rounded-bl-xl  mt-0"
-                  >
-                    Contact Us
-                  </Link>
-                </div>
               </div>
             </div>
           </div>
         </div>
       </section>
 
-      <section className=" text-gray-600  body-font  bg-gray-50 ">
-        <div className="flex flex-col px-5 py-24 items-center justify-center">
-          <h1 className="text-3xl text-2xl font-bold font-serif title-font mb-4 text-gray-800">
-            Why choose us
-          </h1>
-          <p className="leading-relaxed text-2xl">
-            Quality in a service or product is not what you put into it. It’s
-            what the client or customer gets out of it
-          </p>
-          <div className="flex mt-10">
-            <Link
-              to="/Contactus"
-              className="flex items-center w-1/8 justify-center bg-[#14406D] hover:bg-gray-400 hover:text-black text-white font-serif  text-lg border-0 py-1 px-3 focus:outline-none rounded-tr-xl rounded-bl-xl text-base mt-4 md:mt-0"
-            >
-              Connect-Us
-            </Link>
+      <section class="text-gray-600 body-font font-serif bg-gradient-to-r from-cyan-50 via-fuchsia-50 to-blue-50">
+        <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+          <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+            <h1 class="sm:text-4xl text-3xl mb-4  font-semibold text-[#14406D] ">
+              WHY US
+            </h1>
+            <p class="mb-8 leading-relaxed text-lg  ">
+              Our Consulting agency provides Consulting,ideas,and resources for
+              people Working to create social change.we bring the right people
+              together to challenge established thinking and drive
+              transformation.We work with our client to build the capabilities
+              that enable organizations to achieve sustainable advantage.
+            </p>
+          </div>
+          <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+            <img
+              class="object-cover object-center rounded"
+              alt="hero"
+              src={home3}
+            />
           </div>
         </div>
       </section>
 
-      <section className="text-gray-600 body-font">
+      <section className="text-gray-600 body-font font-serif">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
             <img
@@ -169,7 +166,7 @@ function Home() {
             />
           </div>
           <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-semibold font-serif text-[#14406D]">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-semibold  text-[#14406D]">
               Hire Dedicated Developer
             </h1>
             <p className="mb-8 leading-relaxed">
@@ -181,11 +178,26 @@ function Home() {
             <div className="flex justify-center">
               <Link
                 to="/Contactus"
-                className="bg-[#14406D] hover:bg-gray-400 hover:text-black text-white font-serif  text-lg border-0 px-2 focus:outline-none rounded-tr-xl rounded-bl-xl text-base mt-0"
+                className="bg-[#14406D] hover:bg-gray-400 hover:text-black text-white font-serif  text-lg border-0 px-3 py-2 focus:outline-none rounded-tr-xl rounded-bl-xl text-base mt-0"
               >
-                HireNow
+                Hire-Now
               </Link>
             </div>
+          </div>
+        </div>
+      </section>
+
+      <section className={`bg-[url(${home3})] bg-[center_top_69rem] bg-cover`} style={{height:"55vh"}}>
+        <div className="font-serif flex-col sm:flex-row items-center justify-start h-full  w-full py-20   ml-3 sm:ml-4  ">
+          <h1 className="text-start font-bold  text-2xl">WHY US</h1>
+          <div className="container min-w-1/4 flex-col sm:flex-row  text-black">
+            <p className="text-lg">
+              Our Consulting agency provides Consulting,ideas,and resources for
+              people Working to create social change.we bring the right people
+              together to challenge established thinking and drive
+              transformation.We work with our client to build the capabilities
+              that enable organizations to achieve sustainable advantage
+            </p>
           </div>
         </div>
       </section>
@@ -471,12 +483,12 @@ function Home() {
           </div>
         </div>
       </section>
-      
+
       <div className="flex " style={{ position: "relative" }}>
         <img
           src={aboutus}
           alt=""
-          srcset=""
+          srcSet=""
           className="min-w-full  h-3/6 max-h-96 backdrop-blur-xl "
           style={{ filter: " contrast(1.1) brightness(30%) " }}
         />
@@ -494,10 +506,6 @@ function Home() {
           </div>
         </div>
       </div>
-
-  
-
-
     </div>
   );
 }
