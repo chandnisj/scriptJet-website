0f6541d3d377190b5c36c0f31ca1e02ad09fd20b
chandnisj <chandani.scriptjet@gmail.com>
2023-04-20 10:55:58 +0530

    mailing

--- src/Screen/Contactus.jsx
+++ src/Screen/Contactus.jsx
@@ -10,18 +10,20 @@ function Contactus() {
     msg: "",
   });
   const [data, setdata] = useState([]);
+  const [submit, setSubmitted] = useState(false);
   const handalchnage = (event) => {
     setContact({ ...Contact, [event.target.name]: event.target.value });
   };
   const handalclick = (event) => {
     event.preventDefault();
     setdata([...data, Contact]);
+    setSubmitted(true);
     setContact({
       name: "",
       email: "",
       msg: "",
     });
-    //console.log(Contact)
+    console.log(Contact);
   };
   useEffect(() => {
     const targetTop = document.querySelector("#top");
@@ -49,93 +51,107 @@ function Contactus() {
         </div>
       </section>
 
-      <section className="text-gray-600 body-font relative font-serif">
-        <div className="container px-5 py-24  mx-auto  flex flex-wrap ">
-          <div className="lg:w-1/2 md:w-1/2     bg-gray-300 rounded-lg   p-0 flex flex-col items-center justify-start ">
-            <iframe
-              title="map"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1681.5078489552639!2d72.77447927570404!3d21.151529266853636!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be04dab2edc2817%3A0x559ac3e15857f407!2sSNS%20Business%20Park!5e0!3m2!1sen!2sin!4v1679913792009!5m2!1sen!2sin"
-              className="inset-0"
-              style={{ width: "100%", height: "100%", border: "0" }}
-              allowFullScreen=""
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-            ></iframe>
-            <div className="bg-white  flex flex-wrap py-6 rounded shadow-md">
-              <div className=" px-6">
-                <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">
-                  ADDRESS
-                </h2>
-                <p className="mt-1">
-                  305, SNS Business Park, University Rd, opp. J H Ambani School,
-                  Vesu, Surat, Gujarat 395007
-                </p>
+      {!submit && (
+        <section className="text-gray-600 body-font relative font-serif">
+          <div className="container px-5 py-24  mx-auto  flex flex-wrap ">
+            <div className="lg:w-1/2 md:w-1/2     bg-gray-300 rounded-lg   p-0 flex flex-col items-center justify-start ">
+              <iframe
+                title="map"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1681.5078489552639!2d72.77447927570404!3d21.151529266853636!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be04dab2edc2817%3A0x559ac3e15857f407!2sSNS%20Business%20Park!5e0!3m2!1sen!2sin!4v1679913792009!5m2!1sen!2sin"
+                className="inset-0"
+                style={{ width: "100%", height: "100%", border: "0" }}
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
+              <div className="bg-white  flex flex-wrap py-6 rounded shadow-md">
+                <div className=" px-6">
+                  <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">
+                    ADDRESS
+                  </h2>
+                  <p className="mt-1">
+                    305, SNS Business Park, University Rd, opp. J H Ambani
+                    School, Vesu, Surat, Gujarat 395007
+                  </p>
+                </div>
+                <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+                  <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">
+                    EMAIL
+                  </h2>
+                  <a
+                    href="mailto:chandani.scriptjet@gmail.com"
+                    className="text-indigo-500  leading-relaxed"
+                  >
+                  chandani.scriptjet@gmail.com
+                  </a>
+                  <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">
+                    PHONE
+                  </h2>
+                  <p className="leading-relaxed">123-456-7890</p>
+                </div>
               </div>
-              <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
-                <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">
-                  EMAIL
-                </h2>
+            </div>
+            <div className="lg:w-1/3 md:w-1/3  bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+              <h2 className="text-[#14406D] text-2xl mb-1  title-font font-bold font-serif">
+                Contact-Us{" "}
+              </h2>
+              <p className="leading-relaxed mb-5 text-gray-500 font-serif">
+                Don’t hesitate to Contact us for any information.
+              </p>
+              <div className="relative mb-4">
+                <Input
+                  size="large"
+                  placeholder="Name"
+                  prefix={<UserAddOutlined />}
+                  onChange={handalchnage}
+                  name="name"
+                  value={Contact.name}
+                />
+              </div>
+              <div className="relative mb-4">
+                <Input
+                  size="large"
+                  placeholder="Email-Id"
+                  onChange={handalchnage}
+                  name="email"
+                  value={Contact.email}
+                />
+              </div>
+              <div className="relative mb-4">
+                <textarea
+                  id="message"
+                  placeholder="Message"
+                  className="w-full bg-white rounded border border-gray-300  h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                  onChange={handalchnage}
+                  value={Contact.msg}
+                  name="msg"
+                ></textarea>
+              </div>
+              <div className="lg:w-24 md:w-20 sm:w-auto text-center">
                 <a
-                  href="mailto:contact@script-jet.com"
-                  className="text-indigo-500  leading-relaxed"
+                  href={`mailto:contact@script-jet.com?subject=${Contact.name}!&body=${Contact.msg}`}
+                  className="bg-[#14406D] hover:bg-[#FE9800] hover:text-white text-white font-serif  text-lg border-0 py-1 px-3 focus:outline-none rounded-lg mt-4 md:mt-0"
+                  onClick={handalclick}
                 >
-                  contact@script-jet.com
+                  Send
                 </a>
-                <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">
-                  PHONE
-                </h2>
-                <p className="leading-relaxed">123-456-7890</p>
               </div>
             </div>
           </div>
-          <div className="lg:w-1/3 md:w-1/3  bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-            <h2 className="text-[#14406D] text-2xl mb-1  title-font font-bold font-serif">
-              Contact-Us{" "}
-            </h2>
-            <p className="leading-relaxed mb-5 text-gray-500 font-serif">
-              Don’t hesitate to Contact us for any information.
-            </p>
-            <div className="relative mb-4">
-              <Input
-                size="large"
-                placeholder="Name"
-                prefix={<UserAddOutlined />}
-                onChange={handalchnage}
-                name="name"
-                value={Contact.name}
-              />
-            </div>
-            <div className="relative mb-4">
-              <Input
-                size="large"
-                placeholder="Email-Id"
-                onChange={handalchnage}
-                name="email"
-                value={Contact.email}
-              />
-            </div>
-            <div className="relative mb-4">
-              <textarea
-                id="message"
-                placeholder="Message"
-                className="w-full bg-white rounded border border-gray-300  h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                onChange={handalchnage}
-                value={Contact.msg}
-                name="msg"
-              ></textarea>
-            </div>
-            <div className="lg:w-24 md:w-20 sm:w-auto text-center">
-              <a
-                href={`mailto:contact@script-jet.com?subject=${Contact.name}!&body=${Contact.msg}`}
-                className="bg-[#14406D] hover:bg-[#FE9800] hover:text-white text-white font-serif  text-lg border-0 py-1 px-3 focus:outline-none rounded-lg mt-4 md:mt-0"
-                onClick={handalclick}
-              >
-                Send
-              </a>
+        </section>
+      )}
+      {submit && (
+        <section class="text-gray-700 body-font font-serif items-center ">
+          <div class="container px-5 py-24 mx-auto">
+            <div class="lg:w-2/3 flex flex-col sm:flex-row sm:items-center items-start mx-auto">
+              <h1 class="flex-grow sm:pr-16 text-2xl font-medium title-font text-gray-700">
+                Thank You for reaching out to us. Our Team will get back to you
+                soon!
+              </h1>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      )}
     </div>
   );
 }
